c30cd29c92c9ef5324ef6dbcc5e45e53a021aee9
aditya1OG <136246019+aditya1OG@users.noreply.github.com>
2023-07-27 12:55:32 +0530

    Add files via upload

---
+++ CareBooking_Nextjs/components/ShowMore.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import { ShowMoreProps } from "../types";
+import { useRouter } from "next/navigation";
+import { title } from "process";
+import CustomButton from "./CustomButton";
+import { updateSearchParams } from "../utils";
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathName = updateSearchParams("limit", `${newLimit}`);
+    router.push(newPathName);
+  };
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ShowMore;
---
+++ CareBooking_Nextjs/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["cdn.imagin.studio"],
+  },
+  output: 'export'
+};
+
+module.exports = nextConfig;
